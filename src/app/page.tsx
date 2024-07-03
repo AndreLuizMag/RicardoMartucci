77de4b101ed224afb5d9374383de534080464491
André Luiz <andreluizmagjr@outlook.com>
2024-07-03 07:19:16 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+	return (
+		<main>
+			<div className='container-md'>
+				<h1 className='color-blue-regular'>Hello World</h1>
+			</div>
+		</main>
+	)
+}
+
+export default Home
