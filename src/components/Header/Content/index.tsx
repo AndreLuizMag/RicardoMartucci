ca0eb52a34d3a9978e58d484a2e212580a63be77
André Luiz <andreluizmagjr@outlook.com>
2024-07-13 15:26:14 -0300

    Ajuste do menu mobile

--- src/components/Header/Content/index.tsx
+++ src/components/Header/Content/index.tsx
@@ -1,3 +1,4 @@
+import { SocialIcons } from '@/components/SocialIcons'
 import { headerItems } from '@/utils/content'
 import Link from 'next/link'
 import React, { HTMLAttributes } from 'react'
@@ -9,15 +10,18 @@ interface ContentProps
 
 export const Content = () => {
 	return (
-		<div className='content-items flex-bgs ds-flex-center sm:ds-flex-end flow-row-nw sm:flow-col-nw gap-sm sm:p-inline-4 sm:p-block-10'>
+		<div className='content-items flex-bgs ds-flex-center lg:ds-flex-end flow-row-nw lg:flow-col-nw gap-sm lg:p-inline-8 lg:p-block-10'>
 			{headerItems.map((item, index) => (
 				<Link
 					key={index}
 					href={item.href}
-					className='item color-light-primary font-weight-semi-bold text-decoration-none line-height-none p-inline-6 p-block-4 radius-2xs duration-normal property-color ease-in-out hover:bg-blue-lighten hover:color-blue-dark'>
+					className='item lg:width-100 color-light-primary font-weight-semi-bold text-decoration-none text-align-right line-height-none p-inline-6 lg:p-inline-2 p-block-4 radius-2xs duration-normal property-color ease-in-out hover:bg-blue-lighten hover:color-blue-dark'>
 					{item.name}
 				</Link>
 			))}
+			<div className='ds-none lg:ds-block'>
+				<SocialIcons />
+			</div>
 		</div>
 	)
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,13 +5,14 @@ import './styles.scss'
 import Link from 'next/link'
 import { Content } from './Content'
 import { SocialIcons } from '@/components/SocialIcons'
+import { motion, AnimatePresence } from 'framer-motion'
+import { X, List } from '@phosphor-icons/react'
 
 export const Header = () => {
 	const [menuState, setMenuState] = useState(false)
 
 	const handleClick = () => {
-		setMenuState(menuState ? false : true)
-		console.log(menuState)
+		setMenuState(!menuState)
 	}
 
 	return (
@@ -24,23 +25,35 @@ export const Header = () => {
 								<Logo className='logo' />
 							</Link>
 						</div>
-						<div className='col sm:ds-none'>
+						<div className='col lg:ds-none'>
 							<Content />
 						</div>
 						<div className='col ds-flex-end flow-row-nw'>
 							<button
 								type='button'
 								onClick={handleClick}
-								className='ds-none sm:ds-block'>
-								Menu
+								className='ds-none lg:ds-block'>
+								<X size={24} />
+								<List size={24} />
 							</button>
-							<SocialIcons />
+							<div className='lg:ds-none'>
+								<SocialIcons />
+							</div>
 						</div>
 					</div>
 				</div>
-				<div className='content-mobile width-100 ps-absolute ds-none sm:ds-block bg-light-secondary'>
-					<Content />
-				</div>
+				<AnimatePresence>
+					{menuState && (
+						<motion.div
+							initial={{ opacity: 0, y: -16 }}
+							animate={{ opacity: 1, y: 0 }}
+							exit={{ opacity: 0, y: -16 }}
+							transition={{ duration: 0.3 }}
+							className='content-mobile width-100 ps-absolute ds-none lg:ds-block bg-light-secondary'>
+							<Content />
+						</motion.div>
+					)}
+				</AnimatePresence>
 			</nav>
 		</header>
 	)
